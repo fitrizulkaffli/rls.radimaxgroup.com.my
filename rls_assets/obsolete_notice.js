2ffbd67a6837f4dae5f012e06f44710532e39111
Mohd Fitri Zulkaffli <164287053+fitrizulkaffli@users.noreply.github.com>
2025-12-10 12:10:08 +0800

    Update obsolete notice popup content and links

--- rls_assets/obsolete_notice.js
+++ rls_assets/obsolete_notice.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function openPopup() {
         Swal.fire({
-            title: "This Website is Obsolete",
+            title: "Archived Notice",
             icon: "info",
             iconColor: "#FFC107",
-            html: `<br>This website is outdated and has been archived.<br>It no longer reflects the current status or updates and is no longer affiliated with the company.`,
-            showCancelButton: true,
+            html: `<br>You are viewing an archived project.<br>Content and design may be outdated and<br>is no longer affiliated with the original entity.`,
+            showCancelButton: false,
             focusConfirm: false,
             confirmButtonText: `I Understand`,
-            cancelButtonText: `Parent Company Website <i class="fa fa-external-link"></i>`,
-            footer: 'Another website design by&nbsp;<a href="https://fitri.my" target="_blank" rel="noopener noreferrer" class="custom-footer-link">Mohd Fitri Zulkaffli <i class="fa fa-external-link"></i></a>',
-            confirmButtonColor: "#2A52BE",
-            cancelButtonColor: "#24252B",
+            cancelButtonText: `Visit Current Website <i class="fa fa-external-link"></i>`,
+            footer: '© 2024&nbsp;<a href="https://fitri.my" target="_blank" rel="noopener noreferrer" class="custom-footer-link">Mohd Fitri Zulkaffli <i class="fa fa-external-link"></i></a>. All rights reserved.',
+            confirmButtonColor: "#660099",
+            cancelButtonColor: "#09000E",
             allowOutsideClick: false,
             allowEscapeKey: false,
             allowEnterKey: false,
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
             if (result.isConfirmed) {
                 popupOpen = false;
             } else if (result.dismiss === Swal.DismissReason.cancel) {
-                window.open("https://www.radimaxgroup.com.my/", "noopener,noreferrer,nofollow");
+                window.open("https://fitri.my/", "noopener,noreferrer,nofollow");
             }
         });
         popupOpen = true;
